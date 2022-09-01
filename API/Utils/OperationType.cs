cfcc34bfa9ca8c081b99445e75901000a38bf807
lucksma96 <lz.magnanimous@gmail.com>
2022-09-01 13:12:51 -0300

    added operation func to enumeration

--- API/Utils/OperationType.cs
+++ API/Utils/OperationType.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using API.Utils;
+using System.Numerics;
 
 namespace API.Utils
 {
     public class OperationType : Enumeration
     {
-        public static OperationType Addition = new OperationType(1, "addition");
-        public static OperationType Subtraction = new OperationType(2, "subtraction");
-        public static OperationType Multiplication = new OperationType(3, "multiplication");
-        public static OperationType Remainder = new OperationType(4, "remainder");
-        public static OperationType Division = new OperationType(5, "division");
+        public Func<long, long, object> DoOperation { get; set; }
 
-        public OperationType(int id, string name) : base(id, name) { }
+        public static OperationType Addition = new OperationType(1, "addition", (a, b) => a + b);
+        public static OperationType Subtraction = new OperationType(2, "subtraction", (a, b) => a - b);
+        public static OperationType Multiplication = new OperationType(3, "multiplication", (a, b) => BigInteger.Multiply(a, b));
+        public static OperationType Remainder = new OperationType(4, "remainder", (a, b) => a % b);
+        public static OperationType Division = new OperationType(5, "division", (a, b) => a / (decimal)b);
+
+        public OperationType(int id, string name, Func<long, long, object> operationFunc)
+            : base(id, name)
+        {
+            DoOperation = operationFunc;
+        }
     }
 }
 
