ee8a3639267deaa2549f096844b180611c2b9428
lucksma96 <lz.magnanimous@gmail.com>
2022-09-01 13:12:51 -0300

    created task controller

---
+++ API/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Task = API.Models.Task;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    public class TaskController : Controller
+    {
+        private readonly TaskService _taskService;
+
+        public TaskController(TaskService taskService)
+        {
+            _taskService = taskService;
+        }
+
+        [HttpGet("perform")]
+        public async Task<TaskDTO> Perform()
+        {
+            TaskDTO task = await _taskService.GetTaskFromAPI();
+            _taskService.DoTask(task);
+            await _taskService.PostTaskToAPI(task);
+
+            return task;
+        }
+    }
+}
+
