13f0f966f93768f0a722414442f779933b571240
lucksma96 <lz.magnanimous@gmail.com>
2022-09-01 13:12:51 -0300

    created models

---
+++ API/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace API.Models
+{
+    public class Task
+    {
+        public Guid Id { get; set; }
+        public string Operation { get; set; }
+        public long Left { get; set; }
+        public long Right { get; set; }
+        public float Result { get; set; }
+    }
+}
+
---
+++ API/Models/TaskDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using API.Utils;
+
+namespace API.Models
+{
+    public class TaskDTO
+    {
+        public Guid Id { get; set; }
+        public string Operation { get; set; }
+        public long Left { get; set; }
+        public long Right { get; set; }
+        public float? Result { get; set; }
+    }
+}
+
