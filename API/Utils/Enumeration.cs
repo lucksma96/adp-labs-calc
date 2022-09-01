da172888c0632a107649cc4305b57e7fd2d8d0b4
lucksma96 <lz.magnanimous@gmail.com>
2022-09-01 13:12:51 -0300

    created enumerations

---
+++ API/Utils/Enumeration.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace API.Utils
+{
+    /**
+     * Microsoft Docs | Use enumeration classes instead of enum types
+     * https://docs.microsoft.com/en-us/dotnet/architecture/microservices/microservice-ddd-cqrs-patterns/enumeration-classes-over-enum-types
+     */
+    public abstract class Enumeration : IComparable
+    {
+        public string Name { get; private set; }
+
+        public int Id { get; private set; }
+
+        protected Enumeration(int id, string name) => (Id, Name) = (id, name);
+
+        public override string ToString() => Name;
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Enumeration otherValue))
+            {
+                return false;
+            }
+
+            var typeMatches = GetType().Equals(obj.GetType());
+            var valueMatches = Id.Equals(otherValue.Id);
+
+            return typeMatches && valueMatches;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public int CompareTo(object other) => Id.CompareTo(((Enumeration)other).Id);
+
+        public static T FromDisplayName<T>(string displayName) where T : Enumeration
+        {
+            var matchingItem = Parse<T, string>(displayName, "display name", item => item.Name == displayName);
+            return matchingItem;
+        }
+
+        public static IEnumerable<T> GetAll<T>() where T : Enumeration =>
+            typeof(T).GetFields(BindingFlags.Public |
+                                BindingFlags.Static |
+                                BindingFlags.DeclaredOnly)
+                     .Select(f => f.GetValue(null))
+                     .Cast<T>();
+
+        private static T Parse<T, K>(K value, string description, Func<T, bool> predicate) where T : Enumeration
+        {
+            var matchingItem = GetAll<T>().FirstOrDefault(predicate);
+
+            if (matchingItem == null)
+                throw new InvalidOperationException($"'{value}' is not a valid {description} in {typeof(T)}");
+
+            return matchingItem;
+        }
+    }
+}
+
---
+++ API/Utils/OperationType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using API.Utils;
+
+namespace API.Utils
+{
+    public class OperationType : Enumeration
+    {
+        public static OperationType Addition = new OperationType(1, "addition");
+        public static OperationType Subtraction = new OperationType(2, "subtraction");
+        public static OperationType Multiplication = new OperationType(3, "multiplication");
+        public static OperationType Remainder = new OperationType(4, "remainder");
+        public static OperationType Division = new OperationType(5, "division");
+
+        public OperationType(int id, string name) : base(id, name) { }
+    }
+}
+
