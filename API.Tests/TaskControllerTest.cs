a9726bdb79144073583cb897b91de8e5c38b3d33
lucksma96 <lz.magnanimous@gmail.com>
2022-09-01 13:12:51 -0300

    added a test

---
+++ API.Tests/TaskControllerTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Xunit;
+
+namespace API.Tests
+{
+    public class TaskControllerTest
+    {
+    }
+}
+
---
+++ API.Tests/TaskServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using API.Models;
+using API.Services;
+using Xunit;
+
+namespace API.Tests
+{
+    public class TaskServiceTests
+    {
+        [Fact]
+        public void GetTaskFromAPI_Should_ReturnTask()
+        {
+            var taskService = new TaskService();
+
+            var task = taskService.GetTaskFromAPI();
+
+            Assert.IsType<Task>(task);
+            Assert.NotNull(task);
+        }
+    }
+}
+
